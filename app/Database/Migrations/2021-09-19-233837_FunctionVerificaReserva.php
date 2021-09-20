c5273a966732b96b27d94ab7b896bbbf675fe164
AndersonRubel <andersonrubel@gmail.com>
2021-09-19 21:43:02 -0300

    criado tabela de reserva,funcao de valida_reserva e trigger

---
+++ app/Database/Migrations/2021-09-19-233837_FunctionVerificaReserva.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class FunctionVerificaReserva extends Migration
+{
+	public function up()
+	{
+		$this->db->query("CREATE OR REPLACE FUNCTION verifica_reserva() RETURNS pg_catalog.trigger AS $$
+            BEGIN
+
+                 IF NEW.data_inicio > NEW.data_fim THEN
+                    RAISE EXCEPTION 'A Data de início não pode ser maior que a data de término';
+                 END IF;
+
+                 IF NEW.data_inicio < CURRENT_TIMESTAMP THEN
+                    RAISE EXCEPTION 'A Data de início não pode ser menor que a data atual';
+                 END IF;
+
+                 -- verifica se não existe reserva para o imovel no periodo
+                 IF EXISTS (
+                     SELECT 1
+                       FROM reserva
+                      WHERE codigo_imovel = NEW.codigo_imovel
+                        AND ((data_inicio, data_fim) OVERLAPS
+                            (NEW.data_inicio, NEW.data_fim))
+                 )
+                 THEN
+                     RAISE EXCEPTION 'impossível reservar - existe outra reserva para este imovel neste periodo';
+                 END IF;
+                 RETURN NEW;
+            END;
+
+            $$
+            LANGUAGE plpgsql VOLATILE
+        ");
+    }
+
+	public function down()
+	{
+		$this->db->query("DROP FUNCTION verifica_reserva");
+	}
+}
---
+++ app/Database/Migrations/2021-09-20-003032_TriggerVerificaReserva.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class TriggerVerificaReserva extends Migration
+{
+	public function up()
+	{
+		$this->db->query("CREATE TRIGGER verifica_reserva
+                          BEFORE INSERT OR UPDATE ON reserva FOR EACH ROW
+                          EXECUTE PROCEDURE verifica_reserva();");
+	}
+
+	public function down()
+	{
+		$this->db->query("DROP TRIGGER verifica_reserva");
+	}
+}
